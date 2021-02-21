67945697b392623a703a5d7ed3b89d132c9646a0
Defite <me@defite.ru>
2021-02-22 01:56:01 +0300

    fix(index): safari bugs

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container mx-auto flex flex-col justify-center items-center h-screen">
-        <header className="flex flex-col items-center mt-auto">
+      <div className="container mx-auto flex flex-col flex-wrap justify-center items-center h-screen">
+        <header className="mt-auto">
           <h1 className={`font-heading font-bold text-6xl ${styles.logo}`}>
             <div className={styles.logoInner}>
               <div className={styles.up}>
@@ -29,15 +29,15 @@ export default function Home() {
 
         {/* <main></main> */}
 
-        <footer className="flex flex-col items-center justify-center mt-auto">
-          <p className="font-heading text-2xl">Can't touch this</p>
+        <footer className="mt-auto">
+          <p className="font-heading text-2xl text-center">Can't touch this</p>
 
           <div className="flex flex-row justify-center mt-4 mb-8">
-            <img src="./social/facebook.svg" alt="Facebook icon" width="24" className="mx-4" />
-            <img src="./social/github.svg" alt="Github icon" width="24" className="mx-4" />
-            <img src="./social/instagram.svg" alt="Instagram icon" width="24" className="mx-4" />
-            <img src="./social/twitter.svg" alt="Twitter icon" width="24" className="mx-4" />
-            <img src="./social/lastfm.svg" alt="LastFm icon" width="24" className="mx-4" />
+            <img src="./social/facebook.svg" alt="Facebook icon" width="24" height="24" className="mx-4 h-6" />
+            <img src="./social/github.svg" alt="Github icon" width="24" height="24" className="mx-4 h-6" />
+            <img src="./social/instagram.svg" alt="Instagram icon" width="24" height="24" className="mx-4 h-6" />
+            <img src="./social/twitter.svg" alt="Twitter icon" width="24" height="24" className="mx-4 h-6" />
+            <img src="./social/lastfm.svg" alt="LastFm icon" width="24" height="24" className="mx-4 h-6" />
           </div>
         </footer>
       </div>
